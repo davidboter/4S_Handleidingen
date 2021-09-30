5755213338bc1fe8f0995213a06815b7694c6451
davidboter <D278076@edu.rocwb.nl>
2021-09-30 13:57:08 +0200

    fix #10
    Manual download links aangepast naar buttons

--- resources/views/pages/manual_list.blade.php
+++ resources/views/pages/manual_list.blade.php
@@ -19,10 +19,14 @@
 	@foreach ($manuals as $manual)
 	
 		@if ($manual->locally_available)
-			<a href="/{{ $brand->id }}/{{ $brand->name_url_encoded }}/{{ $type->id }}/{{ $type->name_url_encoded }}/{{ $manual->id }}/" alt="{{ __('misc.view_manual_alt') }}" title="{{ __('misc.view_manual_alt') }}">{{ __('misc.view_manual') }}</a> 
+		<div>
+			<a class="linkButton" href="/{{ $brand->id }}/{{ $brand->name_url_encoded }}/{{ $type->id }}/{{ $type->name_url_encoded }}/{{ $manual->id }}/" alt="{{ __('misc.view_manual_alt') }}" title="{{ __('misc.view_manual_alt') }}"><p><span class="bg"></span><span class="base"></span><span class="text">{{ __('misc.view_manual') }}</span></p></a> 
+		</div>
 			({{$manual->filesize_human_readable}})
 		@else
-			<a href="{{ $manual->url }}" target="new" alt="{{ __('misc.download_manual_alt') }}" title="{{ __('misc.download_manual_alt') }}">{{ __('misc.download_manual') }}</a>
+		<div class="button">
+			<a class="linkButton" href="{{ $manual->url }}" target="new" alt="{{ __('misc.download_manual_alt') }}" title="{{ __('misc.download_manual_alt') }}"><p><span class="bg"></span><span class="base"></span><span class="text">{{ __('misc.download_manual') }}</span></p></a>
+		</div>
 		@endif
 		
 		<br />
